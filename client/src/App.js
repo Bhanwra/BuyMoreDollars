a967a638a51ac76ed15f768c0a4b029d0969f276
Dylan <dylanmills@rogers.com>
2021-03-10 20:19:48 -0500

    Added Form Validation!
    ZIP validation is weird, also date of birth is weird.

--- client/src/App.js
+++ client/src/App.js
@@ -17,20 +17,24 @@ function App() {
         <form id="form" onSubmit={formSubmit}>
           <Row>
             <Column width="12">
-              <Input id="name" placeholder="Enter full name" label="Name" errorMessage="Invalid name" />
+              <Input id="name" placeholder="Enter full name" label="Name" errorMessage="Invalid name" type="name" />
             </Column>
             <Column width="12">
-              <Input id="dob" placeholder="Enter date of birth" label="Date of Birth" errorMessage="Invalid date" />
+              <Input id="dob" placeholder="Enter date of birth" label="Date of Birth" errorMessage="Invalid date" type="date" />
             </Column>
             <Column width="12">
-              <Input id="phone" placeholder="Enter phone" label="Phone Number" errorMessage="Invalid phone number" />
+              <Input id="phone" placeholder="Enter phone" label="Phone Number" errorMessage="Invalid phone number" type="phone" />
             </Column>
             <Column width="12">
-              <Input id="address" placeholder="Enter address" label="Address" errorMessage="Invalid address" />
+              <Input id="address" placeholder="Enter address" label="Address" errorMessage="Invalid address" type="address" />
+            </Column>
+            <Column width="12">
+              <Input id="zip" placeholder="Enter ZIP" label="Zip" errorMessage="Invalid zip" type="zip" />
             </Column>
             <Column width="12">
               <Button label="Submit" className="btn-block" type="submit" />
             </Column>
+
           </Row>
         </form>
       </Container>
