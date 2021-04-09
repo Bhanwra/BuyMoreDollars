800e908dd7909fce7ef1ac08429cf137a3cacc7c
Sophie Lamarque <sophielamarque03@gmail.com>
2021-04-09 15:38:43 -0400

    Win page modif

--- client/src/views/Win.jsx
+++ client/src/views/Win.jsx
@@ -4,16 +4,23 @@ import { Link } from 'react-router-dom';
 const Win = (props) => {
 
     return (
-        <main className="bg-gradient-to-b from-white via-green-100 to-white">
-            <h2 className="text-center text-3xl font-bold text-theme-colors-light">Uh-oh!</h2>
-            <p className="text-center font-bold my-5">Looks like it just wasn't your day. <br></br>Better luck next time, friend!</p>
+        <main className="mt-5 flex flex-col items-center">
+            <h2 className=" text-center text-3xl font-bold text-theme-colors-light">Congratulations!</h2>
+            <p className="text-xl text-center mt-5">One more step</p>
+            <p className="text-center mt-1 mb-5">Find the answer to this equation to <br></br>receive your prize.</p>
 
-            <img src={coupon} alt="" className="w-10/12 m-auto"/>
+            <div className="bg-gray-100 w-10/12 p-3">
+                <p className="text-center font-bold text-xl">(2*6) – (20/4)</p>
 
-            <p className="text-center my-5 text-sm">You received a coupon. <br></br>Thank you for playing and supporting BuyMore!</p>
+                <div className="w-10/12 flex justify-items-center m-auto mt-3">
+                    <input type="number"/>
+                    <button type="submit" className="w-4/12 block ml-3">Answer</button>
+                </div>
+            </div>
+            <p className="text-center my-5 text-sm">Thank you for playing and supporting BuyMore!</p>
 
-            <div className="flex justify-center">
-                <Link to="/game" className="w-10/12"><button className="uppercase w-full text-lg m-auto">Back</button></Link>
+            <div className="block w-10/12">
+                <Link to="/game"><button className="uppercase w-full text-lg m-auto">Back</button></Link>
             </div>
         </main>
     )
